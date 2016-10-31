de266d22fab61adf6f70c93ba02d648aa4483baf
Jordan Crawford <jordan.crawford@me.com>
2016-10-31 13:02:26 +1300

    Get the IP address from the local-address-dns-client

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
 var dnsd = require('dnsd');
+var request = require('request');
  
 var server = dnsd.createServer(handleDNSRequest);
 
-// TODO: Make these configurable.
-var TTL = 300; // 5 minutes
-var PORT = 53;
+var TTL = process.env.LOCAL_ADDRESS_DNS_TTL || 300; // default to 5 minutes.
+var PORT = process.env.LOCAL_ADDRESS_DNS_PORT || 53; // default to port 53.
+TTL = parseInt(TTL);
+PORT = parseInt(PORT);
 
-// TODO: Ask the user for the port to use.
+var args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.log('Run with: node server [local address client address]. e.g.: node server http://pi.example.com:3000');
+  return;
+}
+var localAddressClient = args[0];
+
 server.listen(PORT);
 console.log('Server running on port ' + PORT);
+console.log('TTL: ' + TTL);
+console.log('Local address client: ' + localAddressClient);
  
-function handleDNSRequest(request, response) {
-  console.log('%s:%s/%s %j', request.connection.remoteAddress, request.connection.remotePort, request.connection.type, request)
+function handleDNSRequest(dnsRequest, dnsResponse) {
+  console.log(
+    '%s:%s/%s %j',
+    dnsRequest.connection.remoteAddress,
+    dnsRequest.connection.remotePort,
+    dnsRequest.connection.type,
+    dnsRequest);
  
-  var question = response.question[0];
+  var question = dnsResponse.question[0];
   var hostname = question.name;
   
   if (question && question.type === 'A') {
-    // Respond.
-      // TODO: Respond with the correct IP address.
-
-    response.answer.push({
-      name : hostname,
-      type : 'A',
-      data: '192.168.1.17',
-      ttl : TTL
-    });
-    response.end();
+    // Get the IP address of the local address client.
+    try {
+      request(localAddressClient, function (httpError, httpResponse, body) {
+        if (!httpError && httpResponse.statusCode == 200) {
+          var ipAddress = JSON.parse(body).ip;
+          
+          // If a valid IP address was given.
+          if (ipAddress) {
+            dnsResponse.answer.push({
+              name : hostname,
+              type : 'A',
+              data : ipAddress,
+              ttl  : TTL
+            });
+            dnsResponse.end();
+          }
+        } else {
+          console.log('Error with HTTP request.');
+          console.log(httpError);
+        }
+      });
+    } catch (error) {
+      console.log('An error occurred while processing the request.');
+      console.log(error);
+    }
   }
 }
